a33e2f0dd625a03cd6fb78b9d5fc4a7f2413842f
durosly <slyboydon1@gmail.com>
2024-07-26 03:29:27 +0100

    handle file upload properly

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ const express = require("express");
 const app = express();
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" }); Note: by default multer would not supply a file extension for any uploads
+
+/**
+ * To handle displaying file extenstion we use the multer diskStorage method
+ */
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		// this is used to define the path of upload
+		cb(null, "uploads/");
+	},
+	filename: function (req, file, cb) {
+		// this is used to specify the filename
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9); // this is to prevent two files from having the same name
+		const newName = uniqueSuffix + "-" + file.originalname.replace(/\s/g, "-"); // this would create a unique name for the file and also add the file extension
+		cb(null, newName);
+	},
+});
+
+const upload = multer({ storage: storage });
 
 // app.use("/", addUser)
 
